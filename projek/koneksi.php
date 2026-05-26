5b5746baccc37945187f962b3a0062199f94b503
Resty2808 <restydestiani001@gmail.com>
2026-05-26 12:36:42 +0700

    update

--- projek/koneksi.php
+++ projek/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "2526_30db");
+$koneksi = mysqli_connect("localhost", "2526_30, "12345678", "2526_30db");
 if (!$koneksi) {
     die("Gagal konek: " . mysqli_connect_error());
 }
